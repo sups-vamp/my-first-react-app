0b948a291fa925e29fa91afbf9ce5b0feeee7724
sups-vamp <supriya1880.cse16@chitkara.edu.in>
2021-05-28 02:51:37 +0530

    added href

--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand">
+        <a href = "#" className="navbar-brand">
           {props.title}
         </a>
       </div>
